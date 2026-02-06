44c405464496135af2280e75565ea61b3fed6fe9
ivkupenko <i.kupenko@gmail.com>
2026-02-06 18:23:23 +0100

    Request validation for no more then one catch-all for campaign added

---
+++ app/Http/Requests/LandingRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Landing;
+
+class LandingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'country' => 'string|size:2|nullable',
+            'campaign_id' => 'required|exists:campaigns,id',
+            'is_catch_all' => 'required|boolean',
+            'html' => 'required|string',
+        ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            if ($this->input('is_catch_all') && 
+                Landing::where('campaign_id', $this->input('campaign_id'))->where('is_catch_all', true)->exists()) {
+                    $validator->errors()->add('is_catch_all', 'A catch-all landing page already exists for this campaign.');
+            }
+        });
+    }
+}
