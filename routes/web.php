0c936f307c88c2b291507b07ba19d652c251762e
ivkupenko <i.kupenko@gmail.com>
2026-02-06 18:25:20 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,21 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Admin\CampaignController;
+use App\Http\Controllers\Admin\LandingController;
+use App\Http\Controllers\Public\CampaignController as PublicCampaignController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', function () {return view('dashboard');})->middleware('verified')->name('dashboard');
+    Route::resource('/campaigns', CampaignController::class)->except(['show']);
+    Route::resource('/landings', LandingController::class);
 });
+
+Route::get('/campaigns/{slug}', [PublicCampaignController::class, 'show'])->name('campaign.show');
 
 require __DIR__.'/auth.php';
